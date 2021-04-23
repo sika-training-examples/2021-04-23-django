e93e4729e59d4f19742b040e823adb1300d14f58
Ondrej Sika <ondrej@ondrejsika.com>
2021-04-23 09:44:49 +0200

    feat(notes): Add Note model

--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Note(models.Model):
+  title = models.CharField(max_length=100)
+  author = models.ForeignKey("auth.User", on_delete=models.CASCADE)
+  text = models.TextField()
+
+  def __str__(self):
+    return "%s: %s" % (self.author.username, self.title)
