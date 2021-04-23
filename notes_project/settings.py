1b9333a9fae634da12e0df346cd8c49101a53574
Ondrej Sika <ondrej@ondrejsika.com>
2021-04-23 09:25:24 +0200

    feat(settings): Disable password validation for debug

--- notes_project/settings.py
+++ notes_project/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
-AUTH_PASSWORD_VALIDATORS = [
+AUTH_PASSWORD_VALIDATORS = [] if DEBUG else [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
