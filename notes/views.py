39ff335e818de9067bb58018d70dac2b63c456da
Ondrej Sika <ondrej@ondrejsika.com>
2021-04-23 09:55:06 +0200

    fix(notes): Fix order on home page

--- notes/views.py
+++ notes/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from .models import Note
 
 def index_view(request):
-  notes = Note.objects.all()
+  notes = Note.objects.all().order_by("-id")
   return render(request, "notes/index.html", {
     "notes": notes,
   })
